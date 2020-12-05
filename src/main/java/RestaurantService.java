464fcff2a46cefc6acbad45447b8ae9605b76ff8
shekar0391 <shekar0391@gmail.com>
2020-12-06 05:23:03 +0530

    Part 2: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -1,3 +1,6 @@
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -6,10 +9,19 @@ public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
     public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
-    }
+        Restaurant restaurantFound = null;
+        for(Restaurant restaurant: restaurants) {
+            if (restaurant.getName().equals(restaurantName)) {
+                restaurantFound = restaurant;
+                break;
+            }
+        }
 
+        if(restaurantFound == null)
+            throw new RuntimeException(restaurantName);
+
+        return restaurantFound;
+    }
 
     public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
         Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
